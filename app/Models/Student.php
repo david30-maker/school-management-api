2be13d372f40f7e26da6c698fe2bb08747a0da90
david30-maker <davidokpah396@gmail.com>
2025-01-13 11:34:02 +0100

    modify student model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    protected $fillable = ['name', 'age', 'email', 'phone_number', 'class_id', 'address'];
+
+    public function class()
+    {
+        return $this->belongsTo(Classes::class);
+    }
+
+    public function attendances()
+    {
+        return $this->hasMany(Attendance::class);
+    }
 }
