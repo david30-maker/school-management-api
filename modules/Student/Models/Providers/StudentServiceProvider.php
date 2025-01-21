124e9bc67dba82803c9734b3a5dee36dab606b64
david30-maker <davidokpah396@gmail.com>
2025-01-21 17:24:23 +0100

    create service provider

---
+++ modules/Student/Models/Providers/StudentServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Modules\Student\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class StudentServiceProvider extends ServiceProvider
+{
+    public function register() : void
+    {
+        //
+    }
+
+    public function boot() : void
+    {
+        //
+    }
+}
