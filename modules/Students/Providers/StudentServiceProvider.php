4caeef58ae3eb2ec7c01e087d871e94ad583e833
david30-maker <davidokpah396@gmail.com>
2025-01-23 08:34:24 +0100

    service provider

--- modules/Students/Providers/StudentServiceProvider.php
+++ modules/Students/Providers/StudentServiceProvider.php
@@ -16,5 +16,6 @@ class StudentServiceProvider extends ServiceProvider
     public function boot() : void
     {
          $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+         $this->mergeConfigFrom(__DIR__ . '/../Config/config.php', key: 'students');
     }
 }
